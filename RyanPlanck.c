1c7297b9ac4ea021c1a9f1b997bb271200381dd6
Ryan Bega <skuloth@gmail.com>
2016-02-12 11:44:33 -0500

    v1.1

--- RyanPlanck.c
+++ RyanPlanck.c
@@ -1,5 +1,5 @@
 //Ryan Bega - Planck layout
-//v1.0 2/10/16
+//v1.1 2/10/16
 
 #include "planck.h"
 #ifdef BACKLIGHT_ENABLE
@@ -23,22 +23,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_LCTL, KC_LGUI, KC_LALT, KC_TAB, MO(2),  KC_SPC, KC_SPC, MO(3),  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV   }
 },
 [_primary_left] = {
-  {KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELT  },
+  {KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC  },
   {KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_TRNS  },
   {KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS  },
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  }
 },
 [_primary_down] = {
-  {KC_TAB,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DELT    },
-  {KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_TRNS    },
-  {KC_TRNS, KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS    },
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS    }
+  {KC_TAB,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DELT  },
+  {KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_TRNS  },
+  {KC_TRNS, KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS  },
+  {KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  }
 },
 [_primary_up] = {
-  {LALT(KC_F4), KC_NO,               KC_NO,               KC_NO,             KC_NO,                KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_PSCR,  KC_DELT   },
-  {KC_TRNS,     LCTL(LSFT(KC_LEFT)), LCTL(LSFT(KC_DOWN)), LCTL(LSFT(KC_UP)), LCTL(LSFT(KC_RIGHT)), KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS   },
-  {KC_TRNS,     KC_NO,               KC_NO,               KC_NO,             KC_NO,                KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_END,   KC_NO,    KC_TRNS   },
-  {KC_TRNS,     KC_TRNS,             KC_TRNS,             KC_TRNS,           KC_TRNS,              KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGDN,  KC_PGUP,  KC_TRNS,  RESET     }
+  {LALT(KC_F4), KC_F1,               KC_F2,               KC_F3,             KC_F4,                KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_DELT  },
+  {KC_TRNS,     LCTL(LSFT(KC_LEFT)), LCTL(LSFT(KC_DOWN)), LCTL(LSFT(KC_UP)), LCTL(LSFT(KC_RIGHT)), KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F11,   KC_F12,   KC_TRNS  },
+  {KC_TRNS,     KC_NO,               KC_NO,               KC_NO,             KC_NO,                KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_END,   KC_NO,    KC_TRNS  },
+  {KC_TRNS,     KC_TRNS,             KC_TRNS,             KC_TRNS,           KC_TRNS,              KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGDN,  KC_PGUP,  KC_TRNS,  RESET    }
 }
 };
 
